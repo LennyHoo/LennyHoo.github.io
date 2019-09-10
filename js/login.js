2264f92ee03937d8adc748c77c7f10c3ab1fc0ea
LennyHoo <2570171677@qq.com>
2019-09-10 13:58:18 +0800

    2019-9-10-update

---
+++ js/login.js
@@ -0,0 +1,9 @@
+var vm = new Vue({
+    el: "#admin",
+    data: {
+
+    },
+    methods: {
+        
+    },
+})
--- js/main.js
+++ js/main.js
@@ -7,33 +7,50 @@ var vm = new Vue({
                     {tag: "Vue.js", url:"./images/vue_icon.png"},
                     {tag: "Django", url:"./images/django_icon.png"}],
         statistics: {view_num: 0, article_num: 0, comment_num: 0},
-        article_list: [{image_url: "./images/article_img.png", title: "Javascript继承",
-                        intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                         time: "2019-9-8"}, {image_url: "./images/article_img.png", title: "Javascript继承",
-                         intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                          time: "2019-9-8"}, {image_url: "./images/article_img.png", title: "Javascript继承",
-                          intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                           time: "2019-9-8"}, {image_url: "./images/article_img.png", title: "Javascript继承",
-                           intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                            time: "2019-9-8"}, {image_url: "./images/article_img.png", title: "Javascript继承",
-                            intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                             time: "2019-9-8"}, {image_url: "./images/article_img.png", title: "Javascript继承",
-                             intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                              time: "2019-9-8"}, {image_url: "./images/article_img.png", title: "Javascript继承",
-                              intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                               time: "2019-9-8"}, {image_url: "./images/article_img.png", title: "Javascript继承",
-                               intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                                time: "2019-9-8"}, {image_url: "./images/article_img.png", title: "Javascript继承",
-                                intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                                 time: "2019-9-8"}, {image_url: "./images/article_img.png", title: "Javascript继承",
-                                 intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                                  time: "2019-9-8"}, {image_url: "./images/article_img.png", title: "Javascript继承",
-                                  intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                                   time: "2019-9-8"}, {image_url: "./images/article_img.png", title: "Javascript继承",
-                                   intro: "继承是面向对象编程语言的一个特性，子类继承父类的特征和行为，使得子类对象（实例）具有父类的实例域和方法，或子类从父类继承...",
-                                    time: "2019-9-8"}],
+        article_list: [],
+        current_page: 1,
+        all_page_num: 0,
+        mode: "overview",
+    },
+    created() {
+        this.update();
     },
     methods: {
-        
+        update(){
+            this.article_list = [];
+            this.$http.get("http://127.0.0.1:8000/update/1").then(msg => {
+                content = msg.body;
+                content.forEach(element => {
+                    this.article_list.push(element);
+                });
+            });
+            this.$http.get("http://127.0.0.1:8000/statistics").then(msg => {
+                console.log(msg)
+                content = msg.body[0];
+                this.statistics['view_num'] = content['view_num'];
+                this.statistics['article_num'] = content['article_num'];
+                this.statistics['comment_num'] = content['comment_num'];
+                this.all_page_num = content['all_page_num'];
+            })
+        },
+        go_to_page(page_num){
+            if(page_num<1 || page_num > this.all_page_num) return;
+            this.article_list = [];
+            this.$http.get("http://127.0.0.1:8000/update/"+String(page_num)).then(msg => {
+                content = msg.body;
+                content.forEach(element => {
+                    this.article_list.push(element);
+                });
+            });
+            this.current_page = page_num;
+        },
+        to_next_page(){
+            if(this.current_page>=this.all_page_num) return;
+            this.go_to_page(this.current_page+1);
+        },
+        to_front_page(){
+            if(this.current_page<=1) return;
+            this.go_to_page(this.current_page-1);
+        }
     },
 })
